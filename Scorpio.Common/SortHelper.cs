9e04e840e832af452b342794b46437ab4c6328d9
LNBQIAN <geniusiswo@vip.qq.com>
2019-03-26 13:46:47 +0800

    SortHelper

--- Scorpio.Common/SortHelper.cs
+++ Scorpio.Common/SortHelper.cs
@@ -8,25 +8,8 @@ namespace Common
 {
     public class SortHelper
     {
-        //int[] arr = {6, 5, 3, 1, 8, 7, 2, 4};
-
-        public void BubbleSortSort(int[] arr)
-        {
-            //外层循环控制排序趟数
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                //内层循环控制每一趟排序多少次
-                for (int j = 0; j < arr.Length - 1 - i; j++)
-                {
-                    if (arr[j] > arr[j + 1])
-                    {
-                        int temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-                    }
-                }
-            }
-        }
+       
+       
     }
 
 }
